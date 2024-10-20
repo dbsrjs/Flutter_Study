10ce0e03ff1ab25912e867e3365d0a40e27aaadd
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-10-20 22:24:08 +0900

    Create main.dart

---
+++ ROW_COLUMN2[2024-10-19]/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/home_screen.dart';
+
+
+void main(){ 
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
